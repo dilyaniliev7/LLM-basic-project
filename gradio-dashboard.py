d66355a9702965ae6eb434ec9c6f6fba853f0672
dilyaniliev7 <dilyaniliev7@abv.bg>
2025-12-04 21:20:39 +0200

    final functionality added

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -74,3 +74,34 @@ def recommend_books(
 
         if len(authors_split) == 2:
             authors = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])}, and {authors_split[-1]}"
+        else:
+            authors_str = row["authors"]
+
+        caption = f"{row['title']} by {authors_str}: {truncated_description}"
+        results.append((row["large_thumbnail"], caption))
+    return results
+
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices= categories, label="Select a category:", value="All")
+        tone_dropdown = gr.Dropdown(choices= tones, label= "Select an emotional tone:", value="All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label= "Recommended books", columns=8, rows=2)
+
+    submit_button.click(fn = recommend_books,
+                        inputs= [user_query, category_dropdown, tone_dropdown],
+                        outputs = output)
+
+if __name__ == "__main__":
+    dashboard.launch()
